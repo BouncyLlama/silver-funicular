b1eb6b9f4e5c13ab18dba4c50d96efeac956ab07
Ethan Warwick <Ethan.Warwick@natgeo.com>
2019-05-08 13:27:02 -0400

    checkpoint settings

--- testPlugin-server/src/main/kotlin/pages/ProjectTab.kt
+++ testPlugin-server/src/main/kotlin/pages/ProjectTab.kt
@@ -5,15 +5,19 @@ import com.ewarwick.teamcity.testPlugin.settings.ProjectSettings
 import com.ewarwick.teamcity.testPlugin.settings.SettingsManager
 import jetbrains.buildServer.serverSide.ProjectManager
 import jetbrains.buildServer.serverSide.SBuildType
+import jetbrains.buildServer.serverSide.SProject
 import jetbrains.buildServer.users.SUser
 import jetbrains.buildServer.web.openapi.*
 import jetbrains.buildServer.web.openapi.buildType.BuildTypeTab
+import jetbrains.buildServer.web.openapi.project.ProjectTab
 
 import javax.servlet.http.HttpServletRequest
 
-class BuildTab protected constructor(manager: WebControllerManager, projectManager: ProjectManager, descriptor: PluginDescriptor,
-                                     private val settingsManager: SettingsManager) : BuildTypeTab("footabid", "footab", manager, projectManager, descriptor.getPluginResourcesPath(PAGE)) {
-    override fun fillModel(p0: MutableMap<String, Any>, p1: HttpServletRequest, p2: SBuildType, p3: SUser?) {
+class ProjectTab protected constructor(manager: WebControllerManager, projectManager: ProjectManager, descriptor: PluginDescriptor,
+                                       private val settingsManager: SettingsManager) : ProjectTab("footabid", "footab", manager, projectManager, descriptor.getPluginResourcesPath(PAGE)) {
+
+
+    override fun fillModel(p0: MutableMap<String, Any>, p1: HttpServletRequest, p2: SProject, p3: SUser?) {
         p0["Settings"] = settingsManager.pluginSettings
         p0["jspHome"] = jspHome
     }
@@ -30,6 +34,6 @@ class BuildTab protected constructor(manager: WebControllerManager, projectManag
     }
 
     companion object {
-        private val PAGE = "BuildTab.jsp"
+        private val PAGE = "ProjectTab.jsp"
     }
 }
