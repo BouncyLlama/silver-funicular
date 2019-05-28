842b2dc7b27d48934d5d5d51b8a332c5a53816fe
Ethan Warwick <Ethan.Warwick@natgeo.com>
2019-05-28 09:52:06 -0400

    rename

--- testPlugin-server/src/main/kotlin/settings/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.ewarwick.teamcity.testPlugin.settings
-
-class Constants {
-    public val BasePath = "/app/buildtags/api"
-    public val TagEndpoint = BasePath + "/tag"
-
-}
--- testPlugin-server/src/main/kotlin/settings/Settings.kt
+++
@@ -1,7 +0,0 @@
-package com.ewarwick.teamcity.testPlugin.settings
-
-data class ProjectSettings(val patterns: HashMap<String, String> = hashMapOf())
-
-class PluginSettings {
-    val projectSettings: HashMap<String, ProjectSettings> = hashMapOf()
-}
